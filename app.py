315b98e642997f0c5e85e2729274b69595c033d7
Surya Verma <72555711+00surya@users.noreply.github.com>
2021-11-23 13:52:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask,render_template,redirect
 from flask.globals import request
 import joblib
-import os
-
-port = int(os.environ.get('PORT', 5000))
 
 
 model = joblib.load('hark_work_pays_off_model.pkl')
@@ -25,9 +22,10 @@ def marks():
 
 
 
-app.run(host='0.0.0.0', port=port, debug=True)
-# if __name__ == '__main__':
-    # debug = True
+
+if __name__ == '__main__':
+    debug = True
+    app.run(debug=True)
 
 
     
