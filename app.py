9c19c4027d4a8db142519af5e782190a690e1b91
Surya Verma <72555711+00surya@users.noreply.github.com>
2021-11-25 22:37:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,18 @@ from flask import Flask,render_template,redirect
 from flask.globals import request
 import joblib
 
+import sys
+import logging
+
+
 
 model = joblib.load('hark_work_pays_off_model.pkl')
 
 # __name__==__main__
 app = Flask(__name__) #__name__ module name like app.py
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 @app.route('/')
 def hello():
